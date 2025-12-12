870ae0ec0f8396ce7f55dd448cfdb7ea24da824c
Marie Sitar <marie-sitar@uiowa.edu>
2025-12-12 16:48:42 -0600

    created about.html and updated app.py

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home Page Route
 @app.route("/")
-def hello():
+def home():
     return render_template("index.html")
+
+# About Page Route
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
