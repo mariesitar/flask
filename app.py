ebe421e1d3e2c4caa2a4627b5287ee51f41d79f0
Marie Sitar <marie-sitar@uiowa.edu>
2025-12-12 16:51:37 -0600

    added nav to base.html

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home Page Route
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# About Page Route
 @app.route("/about")
 def about_page():
     return render_template("about.html")
