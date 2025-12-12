3fcd30e1fc58911b318fa14dbea193b729d9a344
Marie Sitar <marie-sitar@uiowa.edu>
2025-12-12 16:43:35 -0600

    updated index.html and base.html

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def home():
-    # Instead of returning text, render index.html
+def hello():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
