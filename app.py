0a4f4fab8f39226c55bca40eb5cd3346f2263891
Marie Sitar <marie-sitar@uiowa.edu>
2025-12-12 16:37:14 -0600

    updated Flask app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home():
+    # Instead of returning text, render index.html
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
